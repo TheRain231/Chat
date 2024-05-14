5dd5af8801646ff7428ae7dac1ff299dcf1ecd6e
MRuslanR <rrmukhametshin@ede.hse.ru>
2024-05-14 12:22:54 +0300

    added chat name

--- server/Chat.h
+++ server/Chat.h
@@ -9,6 +9,9 @@ public:
     Chat();
     void add_message(int id, string message);
     vector<pair<int, string>> get_chat();
+    string get_name();
+    void set_name(string name);
 private:
     vector<pair<int, string>>chat;
+    string name;
 };
